9f2f4a5cc9068b7930e05af1cd42d9e02c153c56
hboo <heather.booker@hotmail.com>
2020-09-28 23:56:20 -0400

    Update demo to use new props

--- demo/index.jsx
+++ demo/index.jsx
@@ -4,18 +4,16 @@ var ReactDOM = require('react-dom');
 var WorldMap = require('../dist/index.js');
 
 
-class YourMainComponent extends React.Component {
-  render() {
-    return (
-      <>
-        <h1> The Clickity-Clickable World Map </h1>
-        <WorldMap />
-      </>
-    )
-  }
-}
+function YourMainComponent() {
+  const [selected, setSelected] = React.useState(null);
 
-window.addEventListener('WorldMapClicked', function(e) {console.log('map was clicked, current selection is: ', e.detail.clickedState)});
+  return (
+    <>
+      <h1> The Clickity-Clickable World Map </h1>
+      <WorldMap selected={selected} onSelect={setSelected} />
+    </>
+  );
+}
 
 ReactDOM.render(
   <YourMainComponent />,
